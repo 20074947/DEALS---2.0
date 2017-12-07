ca2aa49016c9fa81669b76b6fb264f8612f218eb
Tarek Huawei <tarek.huawei@yahoo.com>
2017-12-07 16:25:15 +0100

    Refactoring before Webpack

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -5,36 +5,36 @@ app.config(function($routeProvider) {
 
             // route for the home page
             .when('/', {
-                templateUrl : 'pages/home.ejs',
+                templateUrl : 'public/pages/home.ejs',
                 controller  : 'mainController'
             })
 
              // route for the deal page
             .when('/deal', {
-                templateUrl : 'pages/deal.ejs',
+                templateUrl : 'public/pages/deal.ejs',
                 controller  : 'dealController'
             })
             // route for the about page
             .when('/about', {
-                templateUrl : 'pages/about.ejs',
+                templateUrl : 'public/pages/about.ejs',
                 controller  : 'aboutController'
             })
             // route for the about page
             .when('/contact', {
-                templateUrl : 'pages/contact.ejs',
+                templateUrl : 'public/pages/contact.ejs',
                 controller  : 'contactController'
             })
              // route for the deals page
             .when('/deals', {
-                templateUrl : 'pages/deals.ejs',
+                templateUrl : 'public/pages/deals.ejs',
                 controller  : 'dealsController'
             })
 .when('/login', {
-        templateUrl : 'pages/login.ejs',
+        templateUrl : 'public/pages/login.ejs',
         controller  : 'loginController'
     })
 .when('/register', {
-        templateUrl : 'pages/register.ejs',
+        templateUrl : 'public/pages/register.ejs',
         controller  : 'registerController'
     });
     });
